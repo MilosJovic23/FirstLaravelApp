bc5506a19195d662067fc0ad9469ef5fb282378e
MilosJovic23 <milos.mj992@gmail.com>
2025-02-12 12:11:25 +0100

    * first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return "Hello World";
+});
+Route::get('/hello', function () {
+    return "hello";
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+Route::get('/greet/{name}', function ($name) {
+    return "Hello $name"."!";
+});
+Route::fallback(function () {
+    return "Still got somewhere";
+});
