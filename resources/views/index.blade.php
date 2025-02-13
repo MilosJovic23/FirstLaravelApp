2dd753ac807b2f3af452a5be123176a6e89b5af6
MilosJovic23 <milos.mj992@gmail.com>
2025-02-13 13:17:15 +0100

    * creating first blade template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,16 @@
 
+    <h1>The list of tasks</h1>
 
-    <div>hello I'm blade template</div>
+    <div>
 
-@isset($name)
-    <p>The name is : {{ $name }}</p>
-@endisset
+            @forelse($tasks as $task)
+                <div>
+                    <p>{{$task->title}}</p>
+                    <p>{{$task->description}}</p>
+                    <p>{{$task->created_at}}</p>
+                </div>
+                @empty
+                <div>There is no tasks</div>
+           @endforelse
+
+    </div>
