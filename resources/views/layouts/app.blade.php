d19a74fff922437ec53a6c481a56349005bf5ba3
MilosJovic23 <milos.mj992@gmail.com>
2025-02-14 11:10:10 +0100

    * setting up routes for single task

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel 11 Task list App</title>
+    </head>
+
+    <body>
+        <h1>@yield('title')</h1>
+        <div>@yield('content')</div>
+    </body>
+
+</html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,8 +1,18 @@
 
+@extends('layouts.app')
 
+@section('title')
+@endsection
 
-
+@section('content')
         <div>
-            <p>{{$task->title}}</p>
+            <h2>{{ $task->title }}</h2>
+            <p>{{ $task->description }}</p>
+            @if($task->long_description)
+                <p>{{$task->long_description}}</p>
+            @endif
+            <p>{{ $task->created_at }}</p>
+            <p>{{ $task->updated_at }}</p>
         </div>
 
+@endsection
