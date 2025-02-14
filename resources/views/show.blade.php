cfd5a9bc869d70ce807c4854c341f6fd9c01563c
MilosJovic23 <milos.mj992@gmail.com>
2025-02-14 11:14:23 +0100

    * setting up routes for single task

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,18 +1,15 @@
 
 @extends('layouts.app')
 
-@section('title')
-@endsection
+@section('title', $task->title)
 
 @section('content')
-        <div>
-            <h2>{{ $task->title }}</h2>
+
             <p>{{ $task->description }}</p>
             @if($task->long_description)
                 <p>{{$task->long_description}}</p>
             @endif
             <p>{{ $task->created_at }}</p>
             <p>{{ $task->updated_at }}</p>
-        </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -73,18 +73,6 @@ use Illuminate\Support\Facades\Route;
         ]);
     })->name('task.index');
 
-
-//    Route::get('/hello', function () {
-//        return "hello";
-//    })->name('hello');
-//
-//    Route::get('/hallo', function () {
-//        return redirect()->route('hello');
-//    });
-//    Route::get('/greet/{name}', function ($name) {
-//        return "Hello $name"."!";
-//    });
-
     Route::fallback(function () {
         return "Still got somewhere";
     });
