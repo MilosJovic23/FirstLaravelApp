74aa66409527b347128e7d0bfe007c627839c35d
MilosJovic23 <milos.mj992@gmail.com>
2025-02-24 12:27:05 +0100

    * setting up routes for single task

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,12 @@
 
+
+
+@extends('layouts.app')
+
+@section('title', "The list of tasks")
+
     <h1>The list of tasks</h1>
-
-    <div>
-
+@section('content')
             @forelse($tasks as $task)
                 <div>
                     <a href="{{ route('task.show',['id'=>$task->id]) }}">{{$task->title}}</a>
@@ -12,5 +16,5 @@
                 @empty
                 <div>There is no tasks</div>
            @endforelse
+@endsection
 
-    </div>
