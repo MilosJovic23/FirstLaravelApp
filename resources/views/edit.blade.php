ea384b2aab5b7c42252efaa31b0f3a87ef1af931
MilosJovic23 <milos.mj992@gmail.com>
2025-02-28 12:21:49 +0100

    * setting up form data route via post method

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -13,34 +13,34 @@
 @endsection
 
 @section('content')
-    <form method="POST" action="{{ route('task.store') }}">
+    <form method="POST" action="{{ route('task.update',['id'=>$task->id]) }}">
         @csrf
         @method('PUT')
         <div>
             <label for="title">
                 title
             </label>
-            <input name="title" id="title" type="text">
+            <input name="title" id="title" type="text" value="{{$task->title}}">
             @error('title')
             <p class="error-message">{{$message}}</p>
             @enderror
         </div>
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5" >{{$task->description}}</textarea>
             @error('description')
             <p class="error-message">{{$message}}</p>
             @enderror
         </div>
         <div>
-            <label for="long_description">Long_descritpion</label>
+            <label for="long_description">long description</label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
             @error('long_description')
             <p class="error-message">{{$message}}</p>
             @enderror
         </div>
         <div>
-            <button type="submit">add task</button>
+            <button type="submit">Edit task</button>
         </div>
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,24 @@ use Illuminate\Support\Facades\Route;
     })
     ->name('task.store');
 
+    Route::put('/tasks/{id}', function ($id,Request $request)  {
+        $data = ($request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'long_description' => 'required',
+        ]));
+        $task = Task::findOrFail($id);
+        $task->title = $data['title'];
+        $task->description = $data['description'];
+        $task->long_description = $data['long_description'];
+        $task->save();
+
+        return redirect()->route('task.show',['id'=>$task->id])
+            ->with('success','Task updated successfully');
+    })
+        ->name('task.update');
+
+
     Route::fallback(function () {
         return "Still got somewhere";
     });
