11d5655cee6abb5540380906952b615b553170e1
MilosJovic23 <milos.mj992@gmail.com>
2025-03-07 11:44:15 +0100

    * setting up css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,17 @@
     <head>
         <title>Laravel 11 Task list App</title>
         <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+        {{-- blade-formater-disable  --}}
+        <style type="text/tailwindcss">
+            .btn{
+                @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 text-slate-700 ring-slate-700/10 hover:bg-slate-50
+            }
+            .link{
+                @apply font-medium text-gray-700 underline decoration-pink-500
+            }
+        </style>
+        {{-- blade-formater-enable  --}}
         @yield('styles')
     </head>
 
