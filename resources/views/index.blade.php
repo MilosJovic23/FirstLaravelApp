c8b5519f3fa9cd45fe4d991909ae24369f7563a5
MilosJovic23 <milos.mj992@gmail.com>
2025-03-07 11:44:26 +0100

    * setting up css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <nav class="mb-4">
         <a href="{{ route('task.create') }}"
-        class="font-medium text-gray-700 underline decoration-pink-500">Add task</a>
+        class="link">Add task</a>
     </nav>
             @forelse($tasks as $task)
                 <div>
